ced6003753e373fe5afceed6f17064d053f82837
Sergey Arlashin <s.arlashin@gmail.com>
2016-11-18 20:32:15 +0300

    deptest.sh enable

--- deploy/deptest.sh
+++ deploy/deptest.sh
@@ -5,38 +5,46 @@
 
 dep_host='176.9.143.59'
 dep_port='33222'
+
 commit_id=`git log --format="%H" -n 1`
 
 docker_user=${DOCKER_USER}
 docker_pass=${DOCKER_PASS}
 
-ssh -p $dep_port root@$dep_host "docker ps |grep -i testapp"
+appname='testapp'
+
+echo "Check if $appname is running."
+ssh -p $dep_port root@$dep_host "docker ps |grep -i $appname"
 
 if [[ $? -eq 0 ]]; then
-  ssh -p $dep_port root@$dep_host "docker kill testapp"
+  echo "Attempt to stop $appname."
+  ssh -p $dep_port root@$dep_host "docker kill $appname"
   
   if [[ $? -ne 0 ]]; then
-    echo "ERROR: Unable to kill testapp"
+    echo "ERROR: Unable to kill $appname"
     exit 1
   fi 
 fi
 
-ssh  -p $dep_port root@$dep_host "docker ps --all|grep -i testapp"
+echo "Check if $appname container is not removed"
+ssh  -p $dep_port root@$dep_host "docker ps --all|grep -i $appname"
 
 if [[ $? -eq 0 ]]; then
-  ssh -p $dep_port root@$dep_host "docker rm testapp"
+  echo "Remove $appname container"
+  ssh -p $dep_port root@$dep_host "docker rm $appname"
   
   if [[ $? -ne 0 ]]; then
-    echo "ERROR: Unable to remove testapp"
+    echo "ERROR: Unable to remove $appname"
     exit 1
   fi
 fi
 
+echo "Start $appname container"
 ssh -p $dep_port root@$dep_host <<EOF
-  docker run --name testapp -d -p 5000:5000 ${docker_user}/hj_app:$commit_id 
+  docker run --name $appname -d -p 5000:5000 ${docker_user}/hj_app:$commit_id 
 EOF
 
 if [[ $? -ne 0 ]]; then
-  echo "ERROR: Unable to start testapp"
+  echo "ERROR: Unable to start $appname"
   exit 1
 fi
